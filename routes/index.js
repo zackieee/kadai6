6d2c15688df6cbb5a0670b9be5f8ad4781c35832
zackieee <zackie.jackie.222@gmail.com>
2021-01-30 16:53:16 +0900

    同一ドメインでの挙動確認のための処理追加

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    host: req.headers.host,
+  });
+});
+
 /* POST home page. */
 router.post('/', function (req, res, next) {
   res.send({
