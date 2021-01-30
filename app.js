176ee923f7440eac62dd93338062d892097d94b1
zackieee <zackie.jackie.222@gmail.com>
2021-01-30 16:52:20 +0900

    テンプレートエンジンを変更（jade→ejs）

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const corsOptions = {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+//app.set('view engine', 'jade');
 
 app.use(cors(corsOptions));
 
